eabbd15731bdc14e6917d301f3e73f056a8a0460
harrier999 <harrier999@naver.com>
2021-10-27 22:43:53 +0900

    K번쨰수 solved

---
+++ K번째수.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    vector<int> tmp;
+    for (int i =0;i<commands.size();i++){
+        tmp.assign(array.begin() + commands[i][0] - 1, array.begin() + commands[i][1]);
+        sort(tmp.begin(), tmp.end());
+        answer.push_back(tmp.at(commands[i][2] - 1));
+    }
+    return answer;
+}
