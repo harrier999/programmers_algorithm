9f51a614717645e2cc36b3d1aac64f8764d2e4f4
harrier999 <harrier999@naver.com>
2021-10-27 22:48:53 +0900

    K번째수 comment added

--- K번째수.cpp
+++ K번째수.cpp
@@ -1,13 +1,16 @@
 #include <vector>
 #include <algorithm>
 
+/*https://programmers.co.kr/learn/courses/30/lessons/42748*/
+/*프로그래머스 1단계 정렬*/
+
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
-    vector<int> tmp;
+    vector<int> tmp; // Array will be copied to here for maintaining original
     for (int i =0;i<commands.size();i++){
-        tmp.assign(array.begin() + commands[i][0] - 1, array.begin() + commands[i][1]);
+        tmp.assign(array.begin() + commands[i][0] - 1, array.begin() + commands[i][1]); //Copy subset of array
         sort(tmp.begin(), tmp.end());
         answer.push_back(tmp.at(commands[i][2] - 1));
     }
