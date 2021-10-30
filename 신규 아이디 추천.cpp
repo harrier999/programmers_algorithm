359e69cee5459291b7987bb3716259db89be5b40
harrier999 <harrier999@naver.com>
2021-10-30 23:28:30 +0900

    comment added

--- 신규 아이디 추천.cpp
+++ 신규 아이디 추천.cpp
@@ -7,18 +7,18 @@ using namespace std;
 /*40라인 이하로 줄일것*/
 /*https://programmers.co.kr/learn/courses/30/lessons/72410*/
 
-vector<char> string_to_vec(string str) {
+vector<char> string_to_vec(string str) {// 사용하지 말 것
 	vector<char> vec_str(str.begin(), str.end());
 	return vec_str;
 }
-string vec_to_string(vector<char> vec) {
+string vec_to_string(vector<char> vec) { //사용하지 말 것
 	string str = "";
 	for (int i = 0; i < vec.size(); i++) {
 		str.push_back(vec[i]);
 	}
 	return str;
 }
-vector<char> step1(vector<char> str) {
+vector<char> step1(vector<char> str) { //str에 바로 집어넣기
 	vector<char> ret;
 	for (int i = 0; i < str.size(); i++) {
 		ret.push_back(str[i]);
@@ -28,7 +28,7 @@ vector<char> step1(vector<char> str) {
 	return ret;
 }
 
-vector<char> step2(vector<char> str) {
+vector<char> step2(vector<char> str) { // -._ 부분 더 빠르게도 가능하나 충분
 	vector<char> ret;
 	for (int i = 0; i < str.size(); i++) {
 		if ('a' <= str[i] && str[i] <= 'z' || str[i] == '-' || str[i] == '.' || str[i] == '_' || (str[i] <= '9' && str[i] >= '0'))
@@ -36,7 +36,7 @@ vector<char> step2(vector<char> str) {
 	}
 	return ret;
 }
-vector<char> step3plus_4(vector<char> str) {
+vector<char> step3plus_4(vector<char> str) {// 훨씬 빠르게 가능.
 	vector<char> ret;
 	int count = 0;
 	for (int i = 0; i < str.size(); i++) {
@@ -72,7 +72,7 @@ vector<char> step5(vector<char> str) {
 	}
 	return str;
 }
-vector<char> step6(vector<char> str) {
+vector<char> step6(vector<char> str) {//substr 함수 사용하면 빠르게 가능
 	vector<char> ret;
 	if (str.size() > 15) {
 		for (int i = 0; i < 15; i++) {
