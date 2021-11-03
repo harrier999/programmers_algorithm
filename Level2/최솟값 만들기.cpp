0900d2fbab6fdd444a07307971640523c03d29e3
harrier999 <harrier999@naver.com>
2021-11-03 18:17:24 +0900

    최솟값 만들기 solved

---
+++ Level2/최솟값 만들기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+/* 프로그래머스 2단계 연습문제*/
+/*https://programmers.co.kr/learn/courses/30/lessons/12941*/
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), greater<int>());
+    for(int i =0; i<A.size(); i++){
+        answer+= A.at(i) * B.at(i);
+    }
+    
+    
+
+    return answer;
+}
