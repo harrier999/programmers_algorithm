538909978ee5de4eadf6109f9c5c1ccb1c36e300
harrier999 <harrier999@naver.com>
2021-11-10 23:46:08 +0900

    solving 숫자의 표현

---
+++ Level2/숫자의 표현.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+/*프로그래머스 2단계 연습문제*/
+/*https://programmers.co.kr/learn/courses/30/lessons/12924*/
+int solution(int n) {
+    int answer = 0;
+    return answer;
+    //n = (k(자연수의 갯수) * i(시작 자연수))+ (k * (k + 1))/2 - 1
+    /*n   1 * n + 0
+        2 * n + 1
+        3 * n + 3
+        4 * n + 6
+        5 * n + 10
+        6 * n + 15*/
+}
