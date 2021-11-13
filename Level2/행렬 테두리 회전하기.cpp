8b686e86b5a6f4dd903a647ba272cc86229f78b1
harrier999 <harrier999@naver.com>
2021-11-13 23:21:51 +0900

    solving 행렬 테두리 회전하기

---
+++ Level2/행렬 테두리 회전하기.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
+    vector<int> answer;
+    return answer;
+}
