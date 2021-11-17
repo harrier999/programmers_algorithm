594c1f6a37aa80d8d201d49120a7a956650e81c7
harrier999 <harrier999@naver.com>
2021-11-17 22:13:49 +0900

    N-Queen solved

--- Level3/N-Queen.cpp
+++ Level3/N-Queen.cpp
@@ -1,42 +1,52 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
+#include <math.h>
 using namespace std;
 
 
-int answer = 0;
 
-bool is_possible(vector<int> &map, int depth, const int n, int x){
-    for (int i=0; i<depth; i++){
-        if (map[i] == x)
+/*프로그래머스 3단계 연습문제  N-Queen*/
+/*https://programmers.co.kr/learn/courses/30/lessons/12952*/
+int answer = 0; 
+
+bool is_possible(vector<int> map, int depth, const int n, int x) //
+{
+    for (int i = 0; i < depth; i++)
+    {
+        if (map[i] == x || depth - i == abs(x - map[i]))
             return false;
-        if ((map[i] - depth) == x)
     }
     return true;
 }
 
-int set_queen(vector<int> &map, int depth, const int n, int x){
-    int result = 0;
-
-    if (depth == n)
-        return answer++;
-    else{
-        
+void set_queen(vector<int> map, int depth, const int n, int x)
+{
+    map[depth] = x;
+    if (depth == n - 1)
+    {
+        answer = answer + 1;
+        return;
     }
-    for (int x=0; x<n; x++){
-        if (is_possible(map, depth, n + 1, x)){
+
+    for (int x = 0; x < n; x++)
+    {
+        if (is_possible(map, depth + 1, n, x)) 
+        {
             set_queen(map, depth + 1, n, x);
         }
     }
-    
 }
 
-int solution(int n) {
-    int answer = 0;
-    vector<int> map(n, -1);
-    int depth = 0;
-
-
-
+int solution(int n)
+{
+    vector<int> map(n + 1, -1);
+    for (int i=0; i<n; i++){
+        set_queen(map, 0, n, i);
+    }
     return answer;
+}
+int main()
+{
+    solution(4);
 }
